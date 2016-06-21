3c2fff1deb3740cd7d8590867a58667bbaf3fa7d
Josef Krabath <jkrabath.mmt-b2014@fh-salzburg.ac.at>
2016-06-21 21:53:06 +0200

    refactor: changed port of slow.js

--- slow.js
+++ slow.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('server is running on http://localhost:3000');
+app.listen(4000, function() {
+  console.log('server is running on http://localhost:4000');
 });
